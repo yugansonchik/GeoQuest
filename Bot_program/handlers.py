19c1f2ab16c11334836c9f77a22a309f1f0d7481
Natalya Yuganson <72634128+yugansonchik@users.noreply.github.com>
2023-12-11 22:50:27 +0300

    Update handlers.py

--- Bot_program/handlers.py
+++ Bot_program/handlers.py
@@ -3,6 +3,7 @@ from aiogram.types import Message, CallbackQuery, InputMediaPhoto
 from aiogram.filters import CommandStart
 import keyboards
 import Game_formation.start_game as game
+from Levenshtein import distance
 
 # running = 'no' - no games running
 # running = 'russian_cities' - russian cities mode running
@@ -154,16 +155,71 @@ async def process_next_command(message: Message):
 
 
 # Handler for game answers
-@dp.message(lambda message: (GAME_STATUS['running'] != 'no'))
-async def process_game_answer(message: Message):
-    # TODO: Обработка вариативности
-    if message.text == GAME_STATUS[GAME_STATUS['running']]:
+@dp.message(lambda message: (GAME_STATUS['running'] == 'countries'))
+async def process_countries_game_answer(message: Message):
+    correct_answer = GAME_STATUS['countries']
+    user_answer = message.text
+
+    levenshtein_threshold = 3
+
+    # Есть ряд стран, для которых нужно учесть несколько вариантов (вне опечаток, покрываемых расст.Левенштейна)
+    equivalent_names = {
+        'Доминиканская Республика': 'Доминикана',
+        'Кабо-Верде': 'Острова Зеленого Мыса',
+        'Объединенные Арабские Эмираты': ['ОАЭ', 'Арабские Эмираты'],
+        'Республика Конго': 'Конго',
+        'Россия': ['РФ', 'Российская Федерация'],
+        'Сейшельские острова': 'Сейшелы',
+        'Соединенные Штаты Америки': ['США', 'Соединенные Штаты'],
+        'Южно-Африканская Республика': ['Южная Африка', 'ЮАР']
+    }
+
+    equivalent_names_lower = {name.lower(): equivalent_name if isinstance(equivalent_name, str) else equivalent_name[0].lower() for
+                              equivalent_name, names in equivalent_names.items() for name in
+                              ([equivalent_name] if isinstance(equivalent_name, str) else equivalent_name)}
+
+    user_answer_lower = user_answer.lower()
+
+    if user_answer_lower in equivalent_names_lower:
+        if equivalent_names_lower[user_answer_lower] == correct_answer.lower():
+            await message.answer(
+                text=f'Правильно! Это {correct_answer}\n'
+                     'Для нового раунда игры напиши /next. Если хочешь закончить напиши /stop'
+            )
+        else:
+            await message.answer(
+                text=f'Неправильно. Это {correct_answer}\n'
+                     'Для нового раунда игры напиши /next. Если хочешь закончить напиши /stop',
+            )
+        return
+
+    # Сравниваем введенный ответ с правильным по расстоянию Левенштейна
+    if distance(correct_answer.lower(), user_answer_lower) <= levenshtein_threshold:
         await message.answer(
-            text='Правильно! Это ' + GAME_STATUS[GAME_STATUS['running']] + '\n'
+            text=f'Правильно! Это {correct_answer}\n'
                  'Для нового раунда игры напиши /next. Если хочешь закончить напиши /stop'
         )
     else:
         await message.answer(
-            text='Не правильно. Это ' + GAME_STATUS[GAME_STATUS['running']] + '\n' 
+            text=f'Неправильно. Это {correct_answer}\n'
                  'Для нового раунда игры напиши /next. Если хочешь закончить напиши /stop',
         )
+
+'''
+# pip install python-Levenshtein
+
+Эмпирическим путем было выбрано значение параметра threshold=3
+(расстояние Левенштейна 0: точное совпадение между двумя строками, расстояние Левенштейна 1: одно изменение (вставка, удаление или замена одного символа) между строками и так далее)
+
+def check_answer(user_answer, correct_answer, threshold=3):
+    return distance(correct_answer.lower(), user_answer.lower()) <= threshold
+
+correct_answer = "Москва"
+user_answer = "мосакава"
+result = check_answer(user_answer, correct_answer)
+
+if result:
+    print(f"Ответ считается правильным: {user_answer}")
+else:
+    print(f"Ответ считается неправильным: {user_answer}")
+'''
